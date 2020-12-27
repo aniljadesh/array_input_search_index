1605876c6d5ed667a96de399ed1b11631272378f
aniljadesh <58635475+aniljadesh@users.noreply.github.com>
2020-12-27 22:18:40 +0530

    Add files via upload

---
+++ Array_input_search_index.py
@@ -0,0 +1,17 @@
+from array import *
+arr = array('i',[])
+n = int(input("enter the length of array"))
+for i in range(n):
+    x = int(input("enter the next number"))
+    arr.append(x)
+
+print(arr)
+
+val = int(input("enter the search value"))
+
+k = 0
+for e in arr:
+    if e == val:
+        print(k)
+
+k += 1
